ff6d324e95628c7c3e6b6612ca2c166535b2aae0
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-03-02 18:39:35 -0300

    Type void

---
+++ src/A0003-type-void/A0003-type-void.ts
@@ -0,0 +1,16 @@
+function semRetorno(...args: string[]): void {
+  console.log(args.join(' '));
+}
+
+const pessoa = {
+  nome: 'Valdemar',
+  sobrenome: 'Santana',
+
+  exibirNome(): void {
+    console.log(this.nome + ' ' + this.sobrenome);
+  },
+};
+
+semRetorno('Valdemar', 'Santana');
+
+export { pessoa };
