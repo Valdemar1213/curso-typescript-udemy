d8d43b94fd567dc040f4d661cc1db6fdefe5142d
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-03-04 23:27:39 -0300

    type enum

---
+++ src/A0009-type-enum/A0009-type-enum.ts
@@ -0,0 +1,22 @@
+enum Cores {
+  VERMELHO = 10, // 10
+  AZUL = 100, // 100
+  AMARELO = 200, // 200
+}
+
+enum Cores {
+  ROXO = 'ROXO',
+  VERDE = 201,
+  ROSA,
+}
+
+function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaACor(Cores.ROXO);
+
+// console.log(Cores);
+// console.log(Cores.VERMELHO);
+// console.log(Cores[10]);
+// console.log(Cores.ROXO);
