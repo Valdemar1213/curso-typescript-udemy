d6edfd5ad54c383db4ef5c6906f054620be59315
Valdemar Santana <80076890+Valdemar1213@users.noreply.github.com>
2023-03-04 22:52:40 -0300

    tipo tuple

---
+++ src/A0006-type-array/A0006-type-array.ts
@@ -0,0 +1,19 @@
+const dadosCliente1: readonly [number, string] = [1, 'Valdemar'];
+const dadosCliente2: [number, string, string] = [1, 'Valdemar', 'Santana'];
+const dadosCliente3: [number, string, string?] = [1, 'Valdemar'];
+const dadosCliente4: [number, string, ...string[]] = [1, 'Valdemar'];
+
+dadosCliente2[0] = 100;
+dadosCliente2[1] = 'Santana';
+
+console.log(dadosCliente1);
+console.log(dadosCliente2);
+console.log(dadosCliente3);
+console.log(dadosCliente4);
+
+// readonly
+const array: readonly string[] = ['Valdemar', 'Santana'];
+const array1: ReadonlyArray<string> = ['Pedro', 'Santana'];
+
+console.log(array);
+console.log(array1);
